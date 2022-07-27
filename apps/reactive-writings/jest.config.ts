ef080cbdf0d9ef9dcb7a01bc9ac66f16ec41710f
OMGasm <OMGasm@users.noreply.github.com>
2022-07-27 23:04:29 +1000

    initial commit

---
+++ apps/reactive-writings/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'reactive-writings',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/reactive-writings',
+};
